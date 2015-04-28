3bb7434f8ca17e56273ca5d6011a8ce6df44ac22
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2015-04-28 09:22:51 +0300

    Initial Commit

---
+++ TestingStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingStorage
+//
+//  Created by Mohammad AlMarzouq on 4/28/15.
+//  Copyright (c) 2015 KBSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
